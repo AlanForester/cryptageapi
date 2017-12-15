02b67f46d3f2700b47fd83508119aef07bd0a7de
Artem <shkarew@mail.ru>
2017-12-15 17:54:10 +0300

    fix

--- services/routes/crypto/actives.py
+++ services/routes/crypto/actives.py
@@ -6,5 +6,5 @@ from services.auth import requires_auth
 @app.route("/user/actives", methods=["POST"])
 @requires_auth()
 def getActives():
-	data = "{'valid': true, 'data': [{'id': 13, 'name': 'Bitcoin', count: 123},{'id': 1, 'name': 'Зелень', count: 3}]}"
+	data = [{'id': 13, 'name': 'Bitcoin', 'count': 123},{'id': 1, 'name': 'Зелень', 'count': 3}]
 	return jsonify(valid=True, result=data)
